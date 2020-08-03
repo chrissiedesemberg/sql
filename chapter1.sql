41d32047cd5cf98b865ec55d4384882ffb10c27f
“Chrissie <chrizelda.desemberg@gmail.com>
2020-08-03 13:49:32 +0200

    first commit

---
+++ chapter1.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE chrissies_list;
+USE chrissies_list;
+CREATE TABLE chrissies_table
+(
+last_name VARCHAR(30),
+first_name VARCHAR(30),
+email VARCHAR(30),
+birtday DATE,
+prefession VARCHAR(30),
+location VARCHAR(30),
+status VARCHAR(30),
+interests VARCHAR(100),
+seeking VARCHAR(100)
+);
+
+
---
+++ chapter2_queries.sql
@@ -0,0 +1,8 @@
+#SELECT * FROM easy_drinks WHERE main = 'Sprite';       #Returns Lime fizz
+#SELECT * FROM easy_drinks WHERE main = soda;			#Returns error - soda doesnt have ''
+#SELECT * FROM easy_drinks WHERE amount2 = 6;			#Returns Hot Gold and Indian Summer
+#SELECT * FROM easy_drinks WHERE second = "orange juice";#Returns Hot Gold
+#SELECT * FROM easy_drinks WHERE amount1 < 1.5;			#Returns Oh my Gosh
+
+
+
